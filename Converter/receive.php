451657be8f399bb1b7b401d316ba0b6c6e52212e
Liliala <liliala@linux-js73.site>
2014-06-25 15:20:18 -0600

    Convirtiendo

--- Converter/receive.php
+++ Converter/receive.php
@@ -24,17 +24,11 @@ class Archivo extends Illuminate\Database\Eloquent\Model {
 
 $connection = new AMQPConnection('localhost', 5672, 'guest', 'guest');
 $channel = $connection->channel();
-
-
 $channel->queue_declare('hello', false, false, false, false);
-
 echo ' [*] Waiting for messages. To exit press CTRL+C', "\n";
-
 $callback = function($msg) {
   echo " [x] Received ", $msg->body, "\n";
-
   $fila = json_decode($msg->body,true);
-
   foreach ($fila as $key => $value){
     if($key == "origen")
         $origen = $value;
@@ -43,15 +37,11 @@ $callback = function($msg) {
     if($key == "id")
         $id = $value;
   }
-
-  //exec("string");
-   //aki convierto ydespues update
-  Archivo::edit($id,"direccion editar");
-
-  
-
-
-
+  	$nombreArchivo = basename($origen); 
+  	$nombreSinExtension = end(array_reverse(explode(".", $nombreArchivo)));
+  	$nuevoDestino = "/home/liliala/ArchivosMusicBox/Convertidos/$nombreSinExtension.$destino"
+    exec("./ffmpeg -i $origen $nuevoDestino");
+    Archivo::edit($id,"$nuevoDestino");
 };
 
 $channel->basic_consume('hello', '', false, true, false, false, $callback);
